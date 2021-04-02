5e80b7330241fac91f5edcc4f85b99100c29ba93
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-02 19:13:02 +0300

    wired up input redux form

--- components/reduxForm/Input.tsx
+++ components/reduxForm/Input.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { WrappedFieldProps } from "redux-form";
+import styles from "../../styles/authenticate.module.css";
 
-const Input = () => {
-  return <div></div>;
+interface Props {
+  placeholder: string;
+  type: string;
+}
+
+const Input: React.FC<WrappedFieldProps & Props> = ({
+  input,
+  meta,
+  placeholder,
+  type
+}) => {
+  return (
+    <div>
+      <input type={type} placeholder={placeholder} {...input} />
+    </div>
+  );
 };
 
 export default Input;
