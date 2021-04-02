9021c0695150fe3024ca9d1efb6c020d7ea3db31
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-02 12:13:23 +0300

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import { AppProps } from "next/app";
 import { wrapper } from "../redux";
 import "../styles/globals.css";
+import Router from "next/router";
+import nProgress from "nprogress";
+
+(Router as any).onRouteChangeStart = () => {
+  nProgress.start();
+};
+(Router as any).onRouteChangeComplete = () => nProgress.done();
+(Router as any).onRouteChangeError = () => nProgress.done();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
