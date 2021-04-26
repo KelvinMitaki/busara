115164717a975960f50a4b98c5f173778a74a3d5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-26 12:46:50 +0300

    fixed spinner on mobile screens

--- components/layout/Spinner.tsx
+++ components/layout/Spinner.tsx
@@ -10,24 +10,19 @@ interface Props {
 
 const Spinner: React.FC<Props> = ({ context, embeddedInComponent }) => {
   return (
-    <div
-      className={`${styles.container} ${
-        embeddedInComponent ? styles.auth__container : ""
-      }`}
-    >
-      {context === "auth" && (
-        <Sidebar
-          active={
-            typeof window !== "undefined" && Router.route.includes("profile")
-              ? "profile"
-              : "survey"
-          }
-        />
-      )}
-      <div
-        className={styles.main}
-        style={{ ...(context === "unauth" && { width: "100%" }) }}
-      >
+    <div className={`${styles.container} ${embeddedInComponent ? styles.auth__container : ""}`}>
+      <div className={styles.sidebar}>
+        {context === "auth" && (
+          <Sidebar
+            active={
+              typeof window !== "undefined" && Router.route.includes("profile")
+                ? "profile"
+                : "survey"
+            }
+          />
+        )}
+      </div>
+      <div className={styles.main} style={{ ...(context === "unauth" && { width: "100%" }) }}>
         <div className="lds-facebook">
           <div></div>
           <div></div>
